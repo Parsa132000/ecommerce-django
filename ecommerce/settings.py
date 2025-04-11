89e4e9d3b13bcf47593e528776bd79ee6094731d
Parsa132000 <119658054+Parsa132000@users.noreply.github.com>
2025-04-11 20:09:10 +0330

    setting

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = "django-insecure-filj+56!)5!f!v(w#rs9q_#gzyg-@v&un(_&urrj!+^xn@iv%4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ecommerce-django-2gxr.onrender.com', '127.0.0.1', 'localhost']
+
 
 
 # Application definition
